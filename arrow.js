bbf452506931b17868d674ec63e100061c304215
isaiahh02 <102987863+isaiahh02@users.noreply.github.com>
2022-04-29 15:00:23 -0400

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,26 @@
+ arrowMyFunction = (num1, num2) => {
+    return num1 + num2;
+ };
+
+//
+
+arrowAdd100 = (money) => money + 100;
+
+//
+
+ let countLetters = ("Hi I'm Isaiah", "i") 
+     countLetters = 0;
+     for(let i = 0; i < countLetters.length; i++) {
+
+         if(countLetters.charAt (i) === 'p'){
+             count++;
+         }
+     }
+     console.log(countLetters);
+
+
+
+    
+
+
+    
